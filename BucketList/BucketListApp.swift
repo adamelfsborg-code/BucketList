b8ac648b183f2d87030d67e662028f491905c480
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-10 07:46:22 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Adam Elfsborg on 2024-08-10.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
