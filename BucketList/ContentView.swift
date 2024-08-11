47996507560a509e609426417c3c39958018ccf2
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-11 09:43:01 +0200

    feat: core app

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,38 +5,47 @@
 //  Created by Adam Elfsborg on 2024-08-10.
 //
 
-import LocalAuthentication
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isUnlocked = false
-    var body: some View {
-        VStack() {
-            if isUnlocked {
-                Text("Unlocked")
-            } else {
-                Text("Locked")
-            }
-        }
-        .onAppear(perform: authenticate)
-    }
+    let startPosition = MapCameraPosition.region(MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+        span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10))
+    )
     
-    func authenticate() {
-        let context  = LAContext()
-        var error: NSError?
-        
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            let reason = "We need to unlock your data."
-            
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, error in
-                if success {
-                    isUnlocked = true
-                } else {
-                    
+    @State private var locations = [Location]()
+    @State private var selectedLocation: Location?
+    
+    var body: some View {
+        MapReader { proxy in
+            Map(initialPosition: startPosition) {
+                ForEach(locations) { location in
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .clipShape(.circle)
+                            .onLongPressGesture {
+                                selectedLocation = location
+                            }
+                    }
                 }
             }
-        } else {
-            
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local) {
+                    let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                    locations.append(newLocation)
+                }
+            }
+            .sheet(item: $selectedLocation) { location in
+                EditView(location: location) { newLocation in
+                    if let index = locations.firstIndex(of: location) {
+                        locations[index] = newLocation
+                    }
+                }
+            }
         }
     }
 }
