a48743e262820b930085aaacdc9023445a5f28d3
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-11 07:30:34 +0200

    feat: map markers

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -15,10 +15,7 @@ struct Location: Identifiable {
 }
 
 struct ContentView: View {
-    @State private var position = MapCameraPosition.region(MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
-        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-    ))
+
     
     let locations = [
         Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
@@ -27,32 +24,17 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            
-            Map(position: $position)
-                .mapStyle(.hybrid(elevation: .realistic))
-                .onMapCameraChange { context in
-                    print(context.region)
-                }
-            
-            HStack(spacing: 50) {
-                
-                Button("London") {
-                    position = MapCameraPosition.region(MKCoordinateRegion(
-                        center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
-                        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                    ))
-                }
-                Button("Paris") {
-                    position = MapCameraPosition.region(MKCoordinateRegion(
-                        center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
-                        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                    ))
-                }
-                Button("Tokyo") {
-                    position = MapCameraPosition.region(MKCoordinateRegion(
-                        center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
-                        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                    ))
+            Map {
+                ForEach(locations) {location in
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Text(location.name)
+                            .font(.headline)
+                            .padding()
+                            .background(.blue.gradient)
+                            .foregroundStyle(.white)
+                            .clipShape(.capsule)
+                    }
+                    .annotationTitles(.hidden)
                 }
             }
         }
