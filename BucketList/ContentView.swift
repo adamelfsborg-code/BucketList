40ff619b4eeafaba9cea40761b65a425f1536ffd
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-11 08:35:36 +0200

    feat: biometric authenticaiton

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,18 +5,38 @@
 //  Created by Adam Elfsborg on 2024-08-10.
 //
 
+import LocalAuthentication
 import SwiftUI
-import MapKit
 
 struct ContentView: View {
+    @State private var isUnlocked = false
     var body: some View {
-        MapReader { proxy in
-            Map()
-                .onTapGesture { position in
-                    if let coordinate = proxy.convert(position, from: .local) {
-                        print(coordinate)
-                    }
+        VStack() {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context  = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, error in
+                if success {
+                    isUnlocked = true
+                } else {
+                    
                 }
+            }
+        } else {
+            
         }
     }
 }
