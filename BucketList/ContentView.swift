44cab5a75788a4ee9a3a0430724ba251b99dba0e
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-11 07:34:22 +0200

    feat: tap map

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -8,35 +8,15 @@
 import SwiftUI
 import MapKit
 
-struct Location: Identifiable {
-    let id = UUID()
-    var name: String
-    var coordinate: CLLocationCoordinate2D
-}
-
 struct ContentView: View {
-
-    
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076)),
-    ]
-
     var body: some View {
-        VStack {
-            Map {
-                ForEach(locations) {location in
-                    Annotation(location.name, coordinate: location.coordinate) {
-                        Text(location.name)
-                            .font(.headline)
-                            .padding()
-                            .background(.blue.gradient)
-                            .foregroundStyle(.white)
-                            .clipShape(.capsule)
+        MapReader { proxy in
+            Map()
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        print(coordinate)
                     }
-                    .annotationTitles(.hidden)
                 }
-            }
         }
     }
 }
